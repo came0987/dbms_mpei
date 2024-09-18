87fe3c7aa23506f6a70aa8d8d211bc3c8ebde033
Gleb <biazroff@yandex.ru>
2024-09-18 22:57:36 +0300

    add table selection in combobox

--- main.py
+++ main.py
@@ -16,18 +16,28 @@ class ExponatDBMS(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.connection = Data()
-        self.view_table(table_name="vyst_mo")
 
-    def view_table(self, table_name: str):
-        self.model = QSqlTableModel(self)
+        self.ui.vyst_mo_table.setModel(self.create_model(table_name="vyst_mo"))
+        self.ui.vuz_table.setModel(self.create_model(table_name="VUZ"))
+        self.ui.grntirub_table.setModel(self.create_model(table_name="grntirub"))
+        # self.ui.db_tables.setCurrentIndex(0)
+        # self.ui.vuz_table.show()
+        self.ui.tables_combobox.view().pressed.connect(self.set_current_table)
+        # self.set_current_table()
+
+
+
+    def create_model(self, table_name: str):
+        model = QSqlTableModel(self)
+        model.setEditStrategy(QSqlTableModel.EditStrategy.OnManualSubmit)
+        model.setTable(table_name)
+        model.select()
+        return model
         # self.model = QStandardItemModel(self)
         # self.model.setHorizontalHeaderLabels()
-        self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnManualSubmit)
-        self.model.setTable(table_name)
         # self.model.setHeaderData(0, Qt.Orientation.Horizontal, "Name")
         # self.model.setHorizontalHeaderLabels()
-        self.model.select()
-        self.ui.vyst_mo_table.setModel(self.model) #TODO оформить все выше в метод и передавать в параметры
+        # self.ui.vyst_mo_table.setModel(self.model) #TODO оформить все выше в метод и передавать в параметры
         # header = QHeaderView(Qt.Orientation.Horizontal).headerDataChanged()
         # self.ui.vyst_mo_table.setHorizontalHeader()
         # self.ui.db_tables.setCurrentWidget(self.ui.vyst_mo)
@@ -35,10 +45,20 @@ class ExponatDBMS(QMainWindow):
         # self.setCentralWidget(self.ui.vyst_mo_table)
         # self.ui.db_tables.show()
 
+    def set_current_table(self, index):
+        item = self.ui.tables_combobox.model().itemFromIndex(index)
+        if item.text() == "ГРНТИ":
+            self.ui.db_tables.setCurrentIndex(2)
+        if item.text() == "Выставки":
+            self.ui.db_tables.setCurrentIndex(0)
+        if item.text() == "ВУЗы":
+            self.ui.db_tables.setCurrentIndex(1)
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = ExponatDBMS()
     window.show()
-
     sys.exit(app.exec())
